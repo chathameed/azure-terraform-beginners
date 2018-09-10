d3f2360d924b51dab15f36106609cbd476455fa6
Abdul hameed <chathameed@gmail.com>
2018-09-10 20:37:56 +0000

    change 2

--- files/setup.sh
+++ files/setup.sh
@@ -13,6 +13,14 @@ cat << EOM > /var/www/html/index.html
   <marquee><h1>Meow World</h1></marquee>
   </body>
 </html>
+<html>
+  <head><title>Meow!</title></head>
+  <body style="background-image: linear-gradient(red,orange,yellow,green,blue,indigo,violet);">
+  <center><img src="http://placekitten.com/800/600"></img></center>
+  <marquee><h1>Meow World</h1></marquee>
+  </body>
+</html>
+
 EOM
 
 echo "Your demo is now ready."
